fb574ebee49800a879f8b4ab80db7d2472943a1d
Yury Baykov <yury.baykov@yandex.ru>
2017-12-18 02:18:46 +0300

    user is now got from the users db server

---
+++ src/httpSend.ts
@@ -0,0 +1,22 @@
+import fetch, {Response, RequestInit} from "node-fetch";
+
+export default function httpSend(address: string, method: string, body: any = {}): Promise<any> {
+    let requestInit: RequestInit = {
+        method: method,
+        body: (Object.keys(body).length === 0) ? "" : JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    };
+
+    return fetch(address, requestInit)
+        .then((response: Response) => {
+            let jsonPromise = response.json();
+            if (response.status >= 400) {
+                return jsonPromise.then(errorJson => {
+                    throw errorJson;
+                });
+            }
+            return jsonPromise;
+        });
+}
